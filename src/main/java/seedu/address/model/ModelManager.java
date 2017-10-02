353600e7d18c8903096689e92b3ab00ff2191f82
nusjzx <nusjzx@gmail.com>
2017-10-02 13:57:40 +0800

    follow style

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -97,6 +97,7 @@ public class ModelManager extends ComponentManager implements Model {
             addressBook.updatePerson(oldPerson, newPerson);
         }
     }
+
     //=========== Filtered Person List Accessors =============================================================
 
     /**
